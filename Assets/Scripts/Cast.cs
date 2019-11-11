e2cf796cb66a6c8070b4af2307535188f533b5d2
brianbatista <brianfbatista@gmail.com>
2019-11-11 14:16:58 -0500

    Post-hack Duke

---
+++ Assets/Scripts/Cast.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+public class Cast : MonoBehaviour
+{
+    public Camera arCamera;
+    public GameObject window;
+    public GameObject[] scenes;
+    
+    private int opened;
+
+    private void Start()
+    {
+        opened = 0;
+        arCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if (Physics.Raycast(ray, out hitObject)) // true if we hit something
+                {
+                    if (hitObject.transform.tag == "window")
+                    {
+                        if (opened == 0)
+                        {
+                            //this is the rotation script. Change it into your functional code
+                            window.transform.Rotate(0, 90, 0);
+                            opened = 1;
+                        } else
+                        {
+                            window.transform.Rotate(0, -90, 0);
+                            opened = 0;
+                        }
+                    }
+                    
+                }
+            }
+
+            // this is for activating the fog when the condition is right, seems we don't need it for now
+            foreach (GameObject scene in scenes)
+            {
+                if (scene.transform.tag == "fog" && scene.GetComponent < MeshRenderer >().enabled == true)
+                {
+
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Lerping.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerping : MonoBehaviour
+{
+    public float lerpSpeed = .1f;
+
+    [SerializeField]
+    bool rotating = false;
+
+    [SerializeField]
+    float rotationTime;
+
+    private Quaternion targetRotation;
+
+    [SerializeField]
+    bool windowOpen = false;
+
+    private void Start()
+    {
+        targetRotation = transform.rotation;
+    }
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!windowOpen)
+            {
+                targetRotation *= Quaternion.AngleAxis(115, Vector3.up);
+                rotating = true;
+                rotationTime = 0f;
+                windowOpen = true;
+            }
+
+            else
+            {
+                targetRotation *= Quaternion.AngleAxis(-115, Vector3.up);
+                rotating = true;
+                rotationTime = 0f;
+                windowOpen = false;
+            }
+        }
+
+        if (rotating)
+        {
+            rotationTime += Time.deltaTime * lerpSpeed;
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationTime);
+        }
+    }
+}
